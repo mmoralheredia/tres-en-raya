81eaed05f91a04ad18c7b50a9c81c077deac5480
mmoralheredia <132943578+mmoralheredia@users.noreply.github.com>
2023-08-07 01:32:25 -0500

    Cambios de mecanicas y reescribiendo el codigo js

--- src/js/index.js
+++ src/js/index.js
@@ -1,85 +1,68 @@
 import '../scss/index.scss'
-import { randomTurnInit } from "./modules/functions"
 
+// Obtener elementos del DOM a manipular en js.
+const gameScreen = document.querySelector('.screen')
+const buttonInit = document.querySelector('.controls .initAndReset')
+const buttonStop = document.querySelector('.controls .stop')
+const tableBlocks = document.querySelectorAll('.table .block')
 
+// Obtener elementos de las alertas.
+const alertBackground = document.querySelector('.alertBackground')
 
-// Seleccionar los elementos html a manipular.
-const blockList = document.querySelectorAll('.block')
-const screen = document.querySelector('.screen')
-const controlInit = document.querySelector('.controls .initAndReset')
-const controlStop = document.querySelector('.controls .stop ')
+const alertGameMode = document.querySelector('.gameMode')
+const buttonsGameMode = document.querySelectorAll('.gameMode .controls button')
+const buttonCloseGameMode = document.querySelector('.gameMode #closeGameMode')
 
+const alertWinnerOrDraw = document.querySelector('.winnerOrDraw')
+const buttonsWinnerOrDraw = document.querySelectorAll('.winnerOrDraw .controls button')
+const buttonCloseWinnerOrDraw = document.querySelector('.winnerOrDraw #closeWinnerOrDraw')
 
-//Establecer las variables iniciales.
-screen.innerHTML = 'Presione \"Iniciar\"...'
-let turn = ''
+// Declaración de las variables globales del juego
+let currentPlayer = ''
 let gameStatus = false
+let gameTable = ['', '', '', '', '', '', '', '', '']
 
+// Establecer los valores iniciales del juego.
+gameScreen.innerHTML = 'Presione \'Iniciar\'...'
 
+//Configuración de escucha de botones de pantalla principal 'Iniciar' y 'Detener'
+buttonInit.addEventListener('click', () => {
+    console.log("Presionado el botón iniciar")
+    alertBackground.setAttribute('style', 'display: flex;')
+    alertGameMode.setAttribute('style', 'display: flex;')
+})
 
-//Controles de Iniciar y Reiniciar el juego
-controlInit.addEventListener('click', () => {
-    if (controlInit.innerHTML === 'Iniciar') {
-        controlInit.innerHTML = 'Reiniciar'
-        gameStatus = true
-        turn = randomTurnInit()
-        updateScreen()
-    } else if (controlInit.innerHTML === 'Reiniciar') {
-        blockList.forEach(block => { block.innerHTML = '' })
-        turn = randomTurnInit()
-        updateScreen()
-    }
+buttonStop.addEventListener('click', () => {
+    console.log('Botón Detener presionado')
 })
 
 
-//Control de Detener el juego
-controlStop.addEventListener('click', () => {
-    if (gameStatus) {
-        controlInit.innerHTML = 'Iniciar'
-        gameStatus = false
-        turn = ''
-        blockList.forEach(block => {
-            block.innerHTML = ''
-            block.classList = 'block'
-        })
-        updateScreen()
-    }
+// Eventos de escucha del tablero de juego
+tableBlocks.forEach((block, index) => {
+    block.addEventListener('click', function gameActive() {
+        console.log(`Juego activo presiono el bloque ${index + 1}`)
+    })
 })
 
 
-//Control de bloques del tablero, y control del juego activo.
-blockList.forEach((block, index) => {
-    block.addEventListener('click', () => {
-
-        if (gameStatus && turn === 'x' && blockList[index].innerHTML === '') {
-            block.innerHTML = `<img src='src/sources/cross.png' class='blkCross'/>`
-            changesOfTurn()
-            updateScreen()
-        } else if (gameStatus && turn === 'o' && blockList[index].innerHTML === '') {
-            block.innerHTML = `<img src='src/sources/circle.png' class='blkCircle'/>`
-            changesOfTurn()
-            updateScreen()
+//Botones dentro de las alertas de modo de juego.
+buttonsGameMode.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.className === 'playerVsPC') {
+            console.log('Botón de modo de juego Player 1 VS PC, fue presionado')
+            alertBackground.setAttribute('style', 'display: none;')
+            alertGameMode.setAttribute('style', 'display: none;')
+        } else if (button.className === 'playerVsPlayer') {
+            console.log('Botón de modo de juego Player 1 VS Player 2, fue presionado')
+            alertBackground.setAttribute('style', 'display: none;')
+            alertGameMode.setAttribute('style', 'display: none;')
         }
     })
-
 })
 
+buttonCloseGameMode.addEventListener('click', () => {
+    console.log("Botón de cerrar la alerta del \"modo de juego\" presionado")
+    alertBackground.setAttribute('style', 'display: none;')
+    alertGameMode.setAttribute('style', 'display: none;')
+})
 
-//Declaración de Funciones
-function updateScreen() {
-    if (turn === 'x') {
-        screen.innerHTML = 'Turno de X...'
-    } else if (turn === 'o') {
-        screen.innerHTML = 'Turno de O...'
-    } else if (!gameStatus) {
-        screen.innerHTML = 'PRESIONE \"INICIAR\"...'
-    }
-}
-
-function changesOfTurn() {
-    if (turn === 'x') {
-        turn = 'o'
-    } else {
-        turn = 'x'
-    }
-}
